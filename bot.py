a276bd02812c3c00129f255485f875f19fac636c
VuVietDung-bk <dungchuyentoan1@gmail.com>
2026-03-17 00:20:57 +0700

    Fix the chance + texts

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 import random
 import discord
-from discord import app_commands
 from discord.ext import commands
 
 SHITTALK_LINES = [
-    "???",
-    "bro what",
-    "💀",
-    "skill issue",
-    "nah bro tripping",
-    "Goo goo ga ga"
+    "chao cau chu",
+    "goo goo ga ga",
+    "cau chu thang lon roi",
+    "t cho may noi chua",
+    "t cho m cam than chua",
+    "mambo mambo omatsuri mambo",
+    "gogetajob",
+    "t giet",
+    "me long se khoc mat",
+    "stfu nigga",
+    "ngam mom vao, thang hai nua mua",
+    "Nguyen Ha Dong lam duoc thi toi cung lam duoc",
+    "can nha o pho co Ha Noi"
 ]
 
 class ShittalkBot(commands.Bot):
@@ -24,23 +30,33 @@ class ShittalkBot(commands.Bot):
         )
         # Xác suất ban đầu: 1/10
         self._chat_chance = 10
+        # Đếm số lần fail liên tiếp
+        self._fail_count = 0
 
     async def setup_hook(self):
         await self.tree.sync()
         print("Slash commands synced!")
 
     async def on_ready(self):
-        print(f"{self.user} is online! 🌱")
+        print(f"{self.user} is online!")
 
     async def on_message(self, message: discord.Message):
         # Bỏ qua tin nhắn của chính bot
         if message.author == self.user:
             return
 
-        # Thử xác suất hiện tại (1 / self._chat_chance)
-        if random.randint(1, self._chat_chance) == 1:
+        # Sau 97 lần fail, chắc chắn thành công
+        if self._fail_count >= 97:
             await message.channel.send(random.choice(SHITTALK_LINES))
-            # Rerandom tỉ lệ: 1/1000 đến 1/10
-            self._chat_chance = random.randint(10, 1000)
+            self._chat_chance = random.randint(20, 200)
+            self._fail_count = 0
+        # Thử xác suất hiện tại (1 / self._chat_chance)
+        elif random.randint(1, self._chat_chance) == 1:
+            await message.channel.send(random.choice(SHITTALK_LINES))
+            # Rerandom tỉ lệ
+            self._chat_chance = random.randint(20, 200)
+            self._fail_count = 0
+        else:
+            self._fail_count += 1
 
         await self.process_commands(message)
